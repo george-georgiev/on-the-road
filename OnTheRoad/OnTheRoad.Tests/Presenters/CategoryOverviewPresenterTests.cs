ff283c89f75d150cbad1bd719c64e8fe4b739db5
george-georgiev <george.georgiev@mail.bg>
2017-02-08 14:52:09 +0200

    CategoryOverviewPresenterTests added

---
+++ OnTheRoad/OnTheRoad.Tests/Presenters/CategoryOverviewPresenterTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using OnTheRoad.Mvp.CustomControllers.Contracts;
+using OnTheRoad.Mvp.Presenters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTheRoad.Tests.Presenters
+{
+    [TestFixture]
+    public class CategoryOverviewPresenterTests
+    {
+        [Test]
+        public void WhenCategoryOverviewPresenterIsInitialized_WithNull_ArgumentNullException_ShouldBeThrown()
+        {
+            var mockedCategoryOverviewView = new Mock<ICategoryOverviewView>();
+
+            Assert.Throws<ArgumentNullException>(() => new CategoryOverviewPresenter(mockedCategoryOverviewView.Object, null));
+        }
+    }
+}
