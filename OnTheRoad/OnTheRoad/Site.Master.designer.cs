6f3cfa679f07762091923a1835f5e351cf4e5b15
GalinStoychev <stoychev.galin@gmail.com>
2017-02-19 21:37:08 +0200

    SiteMapPath done, and some css

--- OnTheRoad/OnTheRoad/Site.Master.designer.cs
+++ OnTheRoad/OnTheRoad/Site.Master.designer.cs
@@ -40,6 +40,15 @@ namespace OnTheRoad {
         protected global::System.Web.UI.WebControls.LoginView LoginViews;
         
         /// <summary>
+        /// SiteMapPathBreadcrump control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPathBreadcrump;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
