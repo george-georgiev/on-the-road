3509ee8b920b3af4cdc9fa41b1b2e4463380b4aa
GalinStoychev <stoychev.galin@gmail.com>
2017-02-19 16:10:44 +0200

    Review tests fixes after previous commit

--- OnTheRoad/OnTheRoad.Data.Tests/Repositories/ReviewRepositoryTests.cs
+++ OnTheRoad/OnTheRoad.Data.Tests/Repositories/ReviewRepositoryTests.cs
@@ -38,13 +38,38 @@ namespace OnTheRoad.Data.Tests.Repositories
         }
 
         [Test]
+        public void GetByToUser_WhenCalledWithNonExistingUser_ShouldReturnNull()
+        {
+            var nonExistingUser = "Scorpion";
+            var username = "Subz-Zero";
+            var user = new User() { UserName = username };
+
+            var dbSetUserMock = new Mock<DbSet<User>>();
+            contextMock.Setup(x => x.Set<User>()).Returns(dbSetUserMock.Object);
+            var fakeUserData = new List<User>() { user }.AsQueryable();
+            SetDbSetUserAsQueryable(fakeUserData, dbSetUserMock);
+            this.contextMock.Setup(x => x.Users).Returns(dbSetUserMock.Object);
+
+            var reviewRepository = new ReviewRepository(this.contextMock.Object);
+            var actual = reviewRepository.GetByToUser(nonExistingUser);
+
+            Assert.That(actual, Is.Null);
+        }
+
+        [Test]
         public void GetByToUser_WhenCalled_ShouldReturnInstanceOfIEnumerableIReview()
         {
-            var username = "Subz=Zero";
+            var username = "Subz-Zero";
             var user = new User() { UserName = username };
             var review = new Review() { ToUser = user };
-            var fakeData = new List<Review>() { review }.AsQueryable();
-            this.SetDbSetUserAsQueryable(fakeData);
+            var fakeReviewsData = new List<Review>() { review }.AsQueryable();
+            this.SetDbSetReviewAsQueryable(fakeReviewsData);
+
+            var dbSetUserMock = new Mock<DbSet<User>>();
+            contextMock.Setup(x => x.Set<User>()).Returns(dbSetUserMock.Object);
+            var fakeUserData = new List<User>() { user }.AsQueryable();
+            SetDbSetUserAsQueryable(fakeUserData, dbSetUserMock);
+            this.contextMock.Setup(x => x.Users).Returns(dbSetUserMock.Object);
 
             var reviewRepository = new ReviewRepository(this.contextMock.Object);
             var actual = reviewRepository.GetByToUser(username);
@@ -64,7 +89,14 @@ namespace OnTheRoad.Data.Tests.Repositories
             var rating = new Rating() { Value = ratingValue };
             var review = new Review() { ToUser = toUser, FromUser = fromUser, ReviewContent = reviewContent, PostingDate = postDate, Rating = rating };
             var fakeData = new List<Review>() { review }.AsQueryable();
-            this.SetDbSetUserAsQueryable(fakeData);
+            this.SetDbSetReviewAsQueryable(fakeData);
+            
+            var user = new User() { UserName = toUserUsername };
+            var dbSetUserMock = new Mock<DbSet<User>>();
+            contextMock.Setup(x => x.Set<User>()).Returns(dbSetUserMock.Object);
+            var fakeUserData = new List<User>() { user }.AsQueryable();
+            SetDbSetUserAsQueryable(fakeUserData, dbSetUserMock);
+            this.contextMock.Setup(x => x.Users).Returns(dbSetUserMock.Object);
 
             var reviewRepository = new ReviewRepository(this.contextMock.Object);
             var actual = reviewRepository.GetByToUser(toUserUsername).First();
@@ -76,12 +108,20 @@ namespace OnTheRoad.Data.Tests.Repositories
             Assert.That(actual.Rating.Value.Equals(ratingValue));
         }
 
-        private void SetDbSetUserAsQueryable(IQueryable<Review> fakeData)
+        private void SetDbSetReviewAsQueryable(IQueryable<Review> fakeData)
         {
             this.dbSetMock.As<IQueryable<Review>>().Setup(m => m.Provider).Returns(fakeData.Provider);
             this.dbSetMock.As<IQueryable<Review>>().Setup(m => m.Expression).Returns(fakeData.Expression);
             this.dbSetMock.As<IQueryable<Review>>().Setup(m => m.ElementType).Returns(fakeData.ElementType);
             this.dbSetMock.As<IQueryable<Review>>().Setup(m => m.GetEnumerator()).Returns(fakeData.GetEnumerator());
+        }
+
+        private void SetDbSetUserAsQueryable(IQueryable<User> fakeUserData, Mock<DbSet<User>> dbSetUserMock)
+        {
+            dbSetUserMock.As<IQueryable<User>>().Setup(m => m.Provider).Returns(fakeUserData.Provider);
+            dbSetUserMock.As<IQueryable<User>>().Setup(m => m.Expression).Returns(fakeUserData.Expression);
+            dbSetUserMock.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(fakeUserData.ElementType);
+            dbSetUserMock.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(fakeUserData.GetEnumerator());
         }
     }
 }
