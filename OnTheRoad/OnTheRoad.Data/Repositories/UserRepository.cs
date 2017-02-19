e66a2fe20984083cbd0c10c828bb52aec34dc02e
george-georgiev <george.georgiev@mail.bg>
2017-02-19 18:48:55 +0200

    Ignore subscriptions in reviews from user repository

--- OnTheRoad/OnTheRoad.Data/Repositories/UserRepository.cs
+++ OnTheRoad/OnTheRoad.Data/Repositories/UserRepository.cs
@@ -238,7 +238,8 @@ namespace OnTheRoad.Data.Repositories
                     .ForMember(x => x.City, opt => opt.Ignore())
                     .ForMember(x => x.FavouriteUsers, opt => opt.Ignore())
                     .ForMember(x => x.GivenReviews, opt => opt.Ignore())
-                    .ForMember(x => x.ReceivedReviews, opt => opt.Ignore());
+                    .ForMember(x => x.ReceivedReviews, opt => opt.Ignore())
+                    .ForMember(x => x.Subscriptions, opt => opt.Ignore());
 
                 config.CreateMap<Review, IReview>()
                     .ForMember(x => x.Rating, opt => opt.MapFrom(s => Mapper.Map<Rating, IRating>(s.Rating)))
