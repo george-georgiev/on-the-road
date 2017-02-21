ea95e66a0155aa469d183a5f3dcb37f5022a6955
GalinStoychev <stoychev.galin@gmail.com>
2017-02-21 17:43:29 +0200

    TextDataReader tested

---
+++ OnTheRoad/OnTheRoad.Data/Factories/FileReaderFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using OnTheRoad.Data.Factories.Contracts;
+
+namespace OnTheRoad.Data.Factories
+{
+    public class FileReaderFactory : IFileReaderFactory
+    {
+        public TextReader GetStreamReader(string fileName)
+        {
+            return new StreamReader(fileName);
+        }
+    }
+}
