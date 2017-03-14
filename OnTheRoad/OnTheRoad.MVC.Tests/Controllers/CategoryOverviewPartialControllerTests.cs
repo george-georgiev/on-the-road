7f7accc72e70ad081f73610489c8ba47341bc8d1
george-georgiev <george.georgiev@mail.bg>
2017-03-14 02:05:18 +0200

    Added CategoryOverviewPartialController unit tests

---
+++ OnTheRoad/OnTheRoad.MVC.Tests/Controllers/CategoryOverviewPartialControllerTests.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using OnTheRoad.Domain.Models;
+using OnTheRoad.Logic.Contracts;
+using OnTheRoad.MVC.Common;
+using OnTheRoad.MVC.Controllers;
+using OnTheRoad.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace OnTheRoad.MVC.Tests.Controllers
+{
+    [TestFixture]
+    public class CategoryOverviewPartialControllerTests
+    {
+        [Test]
+        public void Constructor_WhenTripGetServiceIsNull_ShouldThrow()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CategoryOverviewPartialController(null));
+        }
+
+        [Test]
+        public void Index_WhenCalled_ShouldReturnPartialViewInstance()
+        {
+            // Arrange
+            var tripServiceMock = new Mock<ITripGetService>();
+            var categoryOverviewPartialController = new CategoryOverviewPartialController(tripServiceMock.Object);
+            string categoryName = "Category Name";
+
+            // Act
+            var result = categoryOverviewPartialController.Index(categoryName);
+
+            // Assert
+            Assert.IsInstanceOf<PartialViewResult>(result);
+        }
+
+        [Test]
+        public void Index_WhenCalled_ShouldReturnCorrectVPartialView()
+        {
+            // Arrange
+            var tripServiceMock = new Mock<ITripGetService>();
+            var categoryOverviewPartialController = new CategoryOverviewPartialController(tripServiceMock.Object);
+            string categoryName = "Category Name";
+
+            // Act
+            var result = categoryOverviewPartialController.Index(categoryName) as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("_CategoryOverviewPartial", result.ViewName);
+        }
+
+        [Test]
+        public void Index_WhenCalled_ShouldReturnCallGetTripsByCategoryNameOrderedByDateOnce()
+        {
+            // Arrange
+            var tripServiceMock = new Mock<ITripGetService>();
+            var categoryOverviewPartialController = new CategoryOverviewPartialController(tripServiceMock.Object);
+            var tripsCount = 4;
+            string categoryName = "Category Name";
+
+            // Act
+            categoryOverviewPartialController.Index(categoryName);
+
+            // Assert
+            tripServiceMock.Verify(x => x.GetTripsByCategoryNameOrderedByDate(It.Is<string>(o => o == categoryName), It.Is<int>(o => o == tripsCount), false), Times.Once);
+        }
+
+        [Test]
+        public void Index_WhenCalled_ShouldSetCorretTripsToModel()
+        {
+            // Arrange
+            var tripServiceMock = new Mock<ITripGetService>();
+            var tripMock = new Mock<ITrip>();
+            var trips = new List<ITrip>() { tripMock.Object };
+            tripServiceMock.Setup(x => x.GetTripsByCategoryNameOrderedByDate(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<bool>())).Returns(trips);
+
+            var tripModelMock = new Mock<TripViewModel>();
+            var autoMapperMock = new Mock<IMapper>();
+            autoMapperMock.Setup(x => x.Map<TripViewModel>(It.IsAny<ITrip>())).Returns(tripModelMock.Object);
+            MapperProvider.Mapper = autoMapperMock.Object;
+
+            var categoryOverviewPartialController = new CategoryOverviewPartialController(tripServiceMock.Object);
+            string categoryName = "Category Name";
+
+            // Act
+            var result = categoryOverviewPartialController.Index(categoryName) as PartialViewResult;
+            var model = result.Model as CategoryOverviewViewModel;
+
+            // Assert
+            Assert.AreSame(tripModelMock.Object, (model.Trips as List<TripViewModel>)[0]);
+        }
+
+        [Test]
+        public void Index_WhenCalled_ShouldSetCorretNameToModel()
+        {
+            // Arrange
+            var tripServiceMock = new Mock<ITripGetService>();
+            var categoryOverviewPartialController = new CategoryOverviewPartialController(tripServiceMock.Object);
+            string categoryName = "Category Name";
+
+            // Act
+            var result = categoryOverviewPartialController.Index(categoryName) as PartialViewResult;
+            var model = result.Model as CategoryOverviewViewModel;
+
+            // Assert
+            Assert.AreSame(categoryName, model.Name);
+        }
+    }
+}
